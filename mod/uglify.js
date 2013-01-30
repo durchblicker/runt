1d7f55cc1fafc714f0dddcd136ecd1864085c9b3
Philipp Dunkel <phidelta@phideltacity.net>
2013-01-30 20:15:06 +0100

    Better Source-Maps

--- mod/uglify.js
+++ mod/uglify.js
@@ -11,13 +11,13 @@ var async = require('async');
 
 function compile(source, target, options, callback) {
   options.documentRoot = path.normalize(this.resolve(options.documentRoot)+'/');
-  options.outSourceMap = path.relative(options.documentRoot, target+'.map');
-  options.sourceRoot = '/';
+  options.outSourceMap = path.basename(target+'.map');
+  //options.sourceRoot = '/';
   var sources = this.source.map(function(s) { return s.path; });
   var result = UglifyJS.minify(sources, options);
   async.forEach([
-    { path:target, content:[ result.code, '//@ sourceMappingURL=/'+options.outSourceMap ].join('\n\n') },
-    { path:target+'.map', content:String(result.map).split(options.documentRoot).join('') }
+    { path:target, content:[ result.code, '//@ sourceMappingURL='+options.outSourceMap ].join('\n\n') },
+    { path:target+'.map', content:String(result.map).split(options.documentRoot).join('/') }
   ], function(file, callback) {
     console.log('Writing('+file.content.length+'): ',file.path);
     fs.writeFile(file.path, file.content, callback);
