74c2baf2cb068e6f9d053156df8c288c394c927e
Philipp Dunkel <p.dunkel@durchblicker.at>
2013-07-08 14:10:49 +0200

    Better Dependency Handling

--- mod/pistachio.js
+++ mod/pistachio.js
@@ -18,17 +18,23 @@ function individual(source, target, options, callback) {
 
 function dependencies(templates, callback) {
   var result = [];
+  var index = {};
   Pea.map(templates, function(file, callback) {
     callback=arguments[arguments.length-1];
     Pea(Pistachio.parse, file).success(function(tpl) {
       var parts = tpl.partials();
       result = result.concat(parts);
+      parts.forEach(function(part) {
+        index[part] = index[part] || [];
+        index[part].push(file);
+        //console.error('Registering '+file+' depends on '+part);
+      });
       callback();
     }).failure(callback);
   }).success(function() {
     var res = {};
     result.forEach(function(file) { res[file]=true; });
     result = Object.keys(res).sort(function(a,b) { return a.length-b.length; });
-    callback(null, result);
+    callback(null, result, index);
   }).failure(callback);
 }
